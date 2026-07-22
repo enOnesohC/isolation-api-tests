f4a1acb2e649952f7d76991ec8f12113dc0d52f5
AndreyEA <enonesohc@mail.ru>
2026-07-22 22:23:16 +0300

    8.2 Ассерты event-driven сценариев_3

--- tests/assertions/grpc/operations.py
+++ tests/assertions/grpc/operations.py
@@ -2,11 +2,11 @@ import allure
 
 from tests.assertions.base import assert_equal
 from tests.clients.postgres.operations.model import OperationsTestModel
-from contracts.services.operations.rpc_get_operation_pb2 import GetOperationResponse
+from contracts.services.operations.operation_pb2 import Operation
 from contracts.services.operations.rpc_get_operations_pb2 import GetOperationsResponse
 from tests.schema.operations import OperationEventTestSchema
 from tests.tools.logger import get_test_logger
-from tests.tools.date import to_proto_test_date
+from tests.tools.date import to_proto_test_datetime
 from tests.mapping.operation_mappings import (
     OPERATION_TYPE_FROM_EVENT,
     OPERATION_STATUS_FROM_EVENT,
@@ -17,7 +17,7 @@ logger = get_test_logger("OPERATIONS_ASSERTIONS")
 
 @allure.step("Check operation from event")
 def assert_operation_from_event(
-        actual: GetOperationResponse,
+        actual: Operation,
         expected: OperationEventTestSchema
 ) -> None:
     logger.info("Check operation from event")
@@ -31,13 +31,13 @@ def assert_operation_from_event(
     assert_equal(actual.user_id, expected.user_id, "user_id")
     assert_equal(actual.card_id, expected.card_id, "card_id")
     assert_equal(actual.category, expected.category, "category")
-    assert_equal(to_proto_test_date(actual.created_at), expected.created_at, "created_at")
+    assert_equal(actual.created_at, to_proto_test_datetime(expected.created_at), "created_at")
     assert_equal(actual.account_id, expected.account_id, "account_id")
 
 
 @allure.step("Check operation from model")
 def assert_operation_from_model(
-        actual: GetOperationResponse,
+        actual: Operation,
         expected: OperationsTestModel
 ) -> None:
     logger.info("Check operation from model")
@@ -52,7 +52,7 @@ def assert_operation_from_model(
     assert_equal(actual.user_id, expected.user_id, "user_id")
     assert_equal(actual.card_id, expected.card_id, "card_id")
     assert_equal(actual.category, expected.category, "category")
-    assert_equal(to_proto_test_date(actual.created_at), expected.created_at, "created_at")
+    assert_equal(actual.created_at, to_proto_test_datetime(expected.created_at), "created_at")
     assert_equal(actual.account_id, expected.account_id, "account_id")
 
 
