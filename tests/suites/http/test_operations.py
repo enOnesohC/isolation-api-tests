7f3941f6547cee33f9a7a9706e62a594236c207a
AndreyEA <enonesohc@mail.ru>
2026-07-23 11:48:11 +0300

    8.3 Event-driven тесты HTTP API_ревью

--- tests/suites/http/test_operations.py
+++ tests/suites/http/test_operations.py
@@ -25,49 +25,14 @@ class TestOperationsHTTP:
             operations_http_test_client: OperationsHTTPTestClient,
             operations_kafka_producer_test_client: OperationsKafkaProducerTestClient
     ):
-        # В этом тесте мы проверяем полный event-driven флоу:
-        # 1. Публикуем событие операции в Kafka.
-        # 2. Ожидаем, что процессор асинхронно обработает событие.
-        # 3. Проверяем результат через синхронный HTTP API.
-        #
-        # Такой тест валидирует интеграцию Kafka, процессинга,
-        # сохранения состояния и HTTP-контракта operations-service.
         event = operations_kafka_producer_test_client.produce_in_progress_purchase_operation_event()
         response = operations_http_test_client.get_operations(user_id=event.user_id)
 
         assert_get_operations_response_from_events(response, [event])
 
-    @pytest.mark.skipif(
-        sys.platform == "win32",
-        reason="psycopg2 + Windows Unicode limitation"
-    )
-    @allure.tag(AllureTag.HTTP, AllureTag.POSTGRES, AllureTag.OPERATIONS_SERVICE)
-    @allure.story(AllureStory.OPERATION_FILTERS)
-    @allure.title("[HTTP][Postgres] Filter by card id. In progress purchase operation")
-    def test_filter_by_card_id_in_progress_purchase_operation(
-            self,
-            operations_http_test_client: OperationsHTTPTestClient,
-            operations_postgres_test_repository: OperationsPostgresTestRepository
-    ):
-        # В этом тесте нас не интересует асинхронный флоу.
-        # Мы проверяем только корректность фильтрации HTTP API.
-        #
-        # Поэтому мы напрямую сидим контролируемое состояние в базу данных,
-        # что делает тест:
-        # - быстрее;
-        # - стабильнее;
-        # - проще для чтения и поддержки.
-        model = operations_postgres_test_repository.create_in_progress_purchase_operation()
-        response = operations_http_test_client.get_operations(
-            user_id=model.user_id,
-            card_id=model.card_id
-        )
-
-        assert_get_operations_response_from_models(response, [model])
-
     @allure.tag(AllureTag.HTTP, AllureTag.KAFKA, AllureTag.OPERATIONS_SERVICE)
     @allure.story(AllureStory.OPERATION_EVENTS)
-    @allure.title("[HTTP][Kafka] Operation events. In completed purchase operation")
+    @allure.title("[HTTP][Kafka] Operation events. Completed purchase operation")
     def test_operation_events_completed_purchase_operation(
             self,
             operations_http_test_client: OperationsHTTPTestClient,
@@ -84,17 +49,36 @@ class TestOperationsHTTP:
     )
     @allure.tag(AllureTag.HTTP, AllureTag.POSTGRES, AllureTag.OPERATIONS_SERVICE)
     @allure.story(AllureStory.OPERATION_FILTERS)
-    @allure.title("[HTTP][Postgres] Filter by card id. In completed purchase operation")
+    @allure.title("[HTTP][Postgres] Filter by card id. In progress purchase operation")
+    def test_filter_by_card_id_in_progress_purchase_operation(
+            self,
+            operations_http_test_client: OperationsHTTPTestClient,
+            operations_postgres_test_repository: OperationsPostgresTestRepository
+    ):
+        model = operations_postgres_test_repository.create_in_progress_purchase_operation()
+        response = operations_http_test_client.get_operations(
+            user_id=model.user_id,
+            card_id=model.card_id
+        )
+
+        assert_get_operations_response_from_models(response, [model])
+
+    @pytest.mark.skipif(
+        sys.platform == "win32",
+        reason="psycopg2 + Windows Unicode limitation"
+    )
+    @allure.tag(AllureTag.HTTP, AllureTag.POSTGRES, AllureTag.OPERATIONS_SERVICE)
+    @allure.story(AllureStory.OPERATION_FILTERS)
+    @allure.title("[HTTP][Postgres] Filter by account id. Completed purchase operation")
     def test_filter_by_account_id_completed_purchase_operation(
             self,
             operations_http_test_client: OperationsHTTPTestClient,
             operations_postgres_test_repository: OperationsPostgresTestRepository
     ):
         model = operations_postgres_test_repository.create_completed_purchase_operation()
-
         response = operations_http_test_client.get_operations(
             user_id=model.user_id,
-            card_id=model.card_id
+            account_id=model.account_id
         )
 
         assert_get_operations_response_from_models(response, [model])
